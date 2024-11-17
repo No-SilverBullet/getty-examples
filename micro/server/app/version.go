51c63bdaa5be1178b91368e505524461ef4f1c40
xinfan.wu(吴歆帆) <xinfan.wu@xgimi.com>
2024-11-17 11:14:52 +0800

    fix:go编译选项去掉-i，新增version文件

---
+++ micro/server/app/version.go
@@ -0,0 +1,16 @@
+/*
+*****************************************************
+# DESC    : echo stream parser
+# AUTHOR  : Alex Stocks
+# LICENCE : Apache License 2.0
+# EMAIL   : alexstocks@foxmail.com
+# MOD     : 2016-09-04 13:08
+# FILE    : readwriter.go
+*****************************************************
+*/
+
+package main
+
+var (
+	Version = "1.0.0"
+)
